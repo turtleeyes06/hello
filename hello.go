af7045cd1ad91dbe14bd821353ad09e42a50f658
Nick carroll <Ncarroll179@gmail.com>
2016-05-10 13:15:29 -0400

    Playing with some functions

--- hello.go
+++ hello.go
@@ -1,7 +1,30 @@
 package main
 
 import "fmt"
+type Unicorn struct{
+	Name string
+	Age int
+	weight int
+}
+
+func (u *Unicorn) SayName()(){
+	fmt.Println(u.Name,"\t",u.Age,"\t",u.weight)	
+}
 
 func main(){
-	fmt.Printf("HELLO WORLD\n")
+	chandara := new(Unicorn)
+	chandara.Name = "Channie"
+	chandara.Age = 21
+	chandara.weight = 130
+
+	//same as
+	nick := &Unicorn{
+	Name:	"Nick Carroll",
+	Age:	28,
+	weight:	200,
 }
+	nick.SayName()
+	chandara.SayName()
+}
+
+
